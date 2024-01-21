339e2b6d3245c092dd6dca9e3e9dcf3d66211d45
njoshi <silwal10kaushal@gmail.com>
2024-01-21 12:05:55 -0600

    initial commit

---
+++ Demo/src/com/kaushal/appone/string/StringBasic.java
@@ -0,0 +1,14 @@
+package com.kaushal.appone.string;
+
+public class StringBasic {
+	
+	
+	public static void main(String[] args) {
+		
+		String name = "John Doe";
+		
+		System.out.println(name);
+		
+	}
+
+}
