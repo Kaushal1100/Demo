c29ed2b8f322635c46dc4420d79ebd55b4cf0e78
njoshi <silwal10kaushal@gmail.com>
2024-01-21 12:43:04 -0600

    assignment_1: intial commit

---
+++ Demo/src/com/kaushal/appone/string/StringSimple.java
@@ -0,0 +1,20 @@
+package com.kaushal.appone.string;
+
+public class StringSimple {
+	
+	
+	public static void main(String[] args) {
+		
+		String name1 = "John Doe";
+		
+		System.out.println(name1);
+		
+		String name2 = "Jane Doe";
+		
+		System.out.println(name2);
+	
+		
+		
+	}
+
+}
